1e4ff333b7f199caeae580e559359d225c1f3636
AAlia16 <67593191+AAlia16@users.noreply.github.com>
2023-02-10 21:43:03 +0100

    Commit DjangoProject

--- polls/views.py
+++ polls/views.py
@@ -13,9 +13,9 @@ class IndexView(generic.ListView):
 
     def get_queryset(self):
         """
-            Return the last five published questions (not including those set to be
-            published in the future).
-            """
+        Return the last five published questions (not including those set to be
+        published in the future).
+        """
         return Question.objects.filter(
             pub_date__lte=timezone.now()
         ).order_by('-pub_date')[:5]
@@ -24,6 +24,12 @@ class IndexView(generic.ListView):
 class DetailView(generic.DetailView):
     model = Question
     template_name = 'polls/detail.html'
+
+    def get_queryset(self):
+        """
+        Excludes any questions that aren't published yet.
+        """
+        return Question.objects.filter(pub_date__lte=timezone.now())
 
 
 class ResultsView(generic.DetailView):
